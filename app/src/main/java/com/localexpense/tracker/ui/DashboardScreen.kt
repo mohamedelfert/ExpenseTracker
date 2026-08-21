396948235a8abfc9a599196487a7d3ef74898090
Mohamed Ibrahiem <mohamedelfert@yahoo.com>
2026-08-21 17:52:28 +0300

    feat: import background modifier in DashboardScreen

--- app/src/main/java/com/localexpense/tracker/ui/DashboardScreen.kt
+++ app/src/main/java/com/localexpense/tracker/ui/DashboardScreen.kt
@@ -1,5 +1,6 @@
 package com.localexpense.tracker.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
